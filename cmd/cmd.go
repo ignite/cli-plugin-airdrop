8a31e7c8991f8c2eeff5d22b8d8781af569699db
Thomas Bruyelle <thomas.bruyelle@gmail.com>
2022-12-13 09:05:48 +0100

    Update cmd/cmd.go
    Co-authored-by: Lucas Bertrand <lucas.bertrand.22@gmail.com>

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -9,7 +9,7 @@ import (
 func NewAirdrop() *cobra.Command {
 	return &cobra.Command{
 		Use:   "airdrop",
-		Short: "Command description...",
+		Short: "Utility tool to create snapshots for an airdrop",
 		RunE: func(cmd *cobra.Command, args []string) error {
 			fmt.Println("Hello from airdrop command")
 			return nil
