b6fbc2ca3d8ae16b413c160958ef58308fc63bfe
Danilo Pantani <danpantani@gmail.com>
2022-12-21 23:11:58 -0300

    remove unused encoding parameters

--- pkg/encode/encode.go
+++ pkg/encode/encode.go
@@ -1,11 +1,9 @@
 package encode
 
 import (
-	"github.com/cosmos/cosmos-sdk/client"
 	"github.com/cosmos/cosmos-sdk/codec"
 	"github.com/cosmos/cosmos-sdk/codec/types"
 	"github.com/cosmos/cosmos-sdk/std"
-	"github.com/cosmos/cosmos-sdk/x/auth/tx"
 	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
 	stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
 )
@@ -14,34 +12,23 @@ import (
 // This is provided for compatibility between protobuf and amino implementations.
 type EncodingConfig struct {
 	InterfaceRegistry types.InterfaceRegistry
-	Marshaler         codec.Codec
-	TxConfig          client.TxConfig
-	Amino             *codec.LegacyAmino
+	Marshaller        codec.Codec
 }
 
 // makeEncodingConfig creates an EncodingConfig for an amino based test configuration.
 func makeEncodingConfig() EncodingConfig {
-	amino := codec.NewLegacyAmino()
 	interfaceRegistry := types.NewInterfaceRegistry()
-	marshaler := codec.NewProtoCodec(interfaceRegistry)
-	txCfg := tx.NewTxConfig(marshaler, tx.DefaultSignModes)
-
 	return EncodingConfig{
 		InterfaceRegistry: interfaceRegistry,
-		Marshaler:         marshaler,
-		TxConfig:          txCfg,
-		Amino:             amino,
+		Marshaller:        codec.NewProtoCodec(interfaceRegistry),
 	}
 }
 
 // MakeEncodingConfig creates an EncodingConfig for testing
 func MakeEncodingConfig() EncodingConfig {
 	encodingConfig := makeEncodingConfig()
-	std.RegisterLegacyAminoCodec(encodingConfig.Amino)
 	std.RegisterInterfaces(encodingConfig.InterfaceRegistry)
-	banktypes.RegisterLegacyAminoCodec(encodingConfig.Amino)
 	banktypes.RegisterInterfaces(encodingConfig.InterfaceRegistry)
-	stakingtypes.RegisterLegacyAminoCodec(encodingConfig.Amino)
 	stakingtypes.RegisterInterfaces(encodingConfig.InterfaceRegistry)
 	return encodingConfig
 }
@@ -49,5 +36,5 @@ func MakeEncodingConfig() EncodingConfig {
 // Codec creates a new Codec
 func Codec() codec.Codec {
 	encodingConfig := MakeEncodingConfig()
-	return encodingConfig.Marshaler
+	return encodingConfig.Marshaller
 }
